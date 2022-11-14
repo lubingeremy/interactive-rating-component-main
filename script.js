36e997ea9f52d1f5507c63397943be7a53dd9bc4
Gérémy Lubin <97472153+lubingeremy@users.noreply.github.com>
2022-11-13 20:19:38 -0400

    Design, animation, script

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 
+const form = document.getElementById("myForm")
 const rating = document.getElementById("rating")
 const thanking = document.getElementById("thanking")
 const submit = document.getElementById("submitButton")
+
 var answer = 0
+
 var submittedRating = document.getElementById("submittedRating")
 
-// console.log(rating, thanking)
+const propositions = ["optionOne", "optionTwo", "optionThree", "optionFour", "optionFive"]
 
 function setAnswer(rating){
   answer = rating 
-  console.log(answer)
+}
+
+function cardTransition(){
+  rating.classList.add("slideOut")
+  thanking.classList.add("slideIn")
 }
 
 function showThanks(){
-  event.preventDefault()
-  rating.style.display = "none"
-  thanking.style.display = "flex"
+  cardTransition()
   submittedRating.innerText = answer
+  resetRadio()
 }
+
+function resetRadio(){
+  for (index in propositions){
+    document.getElementById(propositions[index]).checked = false;
+  }
+}
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  answer = document.querySelector("input[name=ratingOption]:checked").value;
+  showThanks()
+})
